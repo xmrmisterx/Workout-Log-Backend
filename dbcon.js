5009c70c15d3322603570fdd5728d58bb218e66e
Bobby Nguyen <nguyenbo@flip1.engr.oregonstate.edu>
2021-09-06 13:48:44 -0700

    saved now uploading clearDB config

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
+
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs290_nguyenbo',
-  password        : '8248',
-  database        : 'cs290_nguyenbo',
+  host            : 'us-cdbr-east-04.cleardb.com',
+  user            : 'ba78fb274f2846',
+  password        : 'e7c84551',
+  database        : 'heroku_2502c22c3414591',
 });
+
+// var pool = mysql.createPool({
+//   connectionLimit : 10,
+//   host            : 'classmysql.engr.oregonstate.edu',
+//   user            : 'cs290_nguyenbo',
+//   password        : '8248',
+//   database        : 'cs290_nguyenbo',
+// });
 
 module.exports.pool = pool;
